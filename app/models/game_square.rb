b4119549b1d2d5b453d118bb9c6b243377273b9e
TedCart <TedCart@gmail.com>
2018-01-26 16:17:52 -0500

    Removed game_squares scaffold

--- app/models/game_square.rb
+++
@@ -1,3 +0,0 @@
-class GameSquare < ApplicationRecord
-  belongs_to :game
-end
--- app/serializers/game_square_serializer.rb
+++
@@ -1,4 +0,0 @@
-class GameSquareSerializer < ActiveModel::Serializer
-  attributes :id, :cell_index, :value
-  has_one :game
-end
