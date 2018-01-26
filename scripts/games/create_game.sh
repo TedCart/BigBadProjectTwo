cf8e34e4794b9affee4e264fb42fe211348474bb
TedCart <TedCart@gmail.com>
2018-01-26 13:28:17 -0500

    Minor edit to create game curl script

--- scripts/games/create_game.sh
+++ scripts/games/create_game.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# TOKEN=XxX sh scripts/games/create_game.sh
+# TOKEN=BAhJIiU1NmJiZTI0ZjIzYzYxMzNlOWE1ZWMxYmM4Y2M5MDBmNwY6BkVG--e203459ae70daf03bf63fe8fe118f0b5ad44fb59 sh scripts/games/create_game.sh
 
 curl "http://localhost:4741/games" \
   --include \
