df659512142b65191a9ad3b6ac13511c6d3fdada
TedCart <TedCart@gmail.com>
2018-01-26 10:04:16 -0500

    Added copy/paste comment line in users index curl

--- scripts/users/users.sh
+++ scripts/users/users.sh
@@ -6,3 +6,5 @@ curl "http://localhost:4741/users" \
   --header "Authorization: Token token=${TOKEN}"
 
 echo
+
+# TOKEN=BAhJIiUwNmZlNzRhYWIyOWU5NmZmMTg1ZGZiM2ZlYzMyNjAzNAY6BkVG--dcc3bedef46f5d7df696d9977375d8e1a1f0b609 sh scripts/users/users.sh
