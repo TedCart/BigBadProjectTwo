3733d0fc75c01bdfccd7348758d9b2395b6e1213
TedCart <TedCart@gmail.com>
2018-02-11 12:39:26 -0500

    Dropped Words Table
    Also change games_controller from OpenReadController to ProtectedController

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126220817) do
+ActiveRecord::Schema.define(version: 20180211173524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,14 +40,6 @@ ActiveRecord::Schema.define(version: 20180126220817) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
-  end
-
-  create_table "words", force: :cascade do |t|
-    t.integer "game_id"
-    t.integer "player_id"
-    t.string "word"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "examples", "users"
